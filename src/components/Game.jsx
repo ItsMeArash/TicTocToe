2f25ebb0807f6663783bb83bca9b32ffc22d5dfd
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2023-05-20 18:08:37 +0330

    Moves history feature added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -8,34 +8,40 @@ const styles = {
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const clickHandler = (i) => {
-    const boardCopy = [...board];
-    if (winner || boardCopy[i]) return;
-    boardCopy[i] = xIsNext ? "X" : "O";
-    setBoard(boardCopy);
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
+    if (winner || squares[i]) return;
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXIsNext(!xIsNext);
   };
 
-  const jumpTo = () => {};
-
-  const renderMoves = () => {
-    return (
-      <button
-        onClick={() => {
-          setBoard(Array(9).fill(null));
-        }}
-      >
-        Restart Game
-      </button>
-    );
+  const jumpTo = step => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0)
   };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move${move}` : "Go to start";
+      return (
+        <li key={move}>
+            <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+        
+      );
+    });
   return (
     <>
-      <Board squares={board} onClick={clickHandler} />
+      <Board squares={history[stepNumber]} onClick={clickHandler} />
       <div style={styles}>
         <p>
           {winner ? `winner: ${winner}` : `Next Player: ${xIsNext ? "X" : "O"}`}
