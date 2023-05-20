2e78c87937999f40cf93213528954c2ca23a1719
Arash Harandehi <56218691+ItsMeArash@users.noreply.github.com>
2023-05-20 17:37:15 +0330

    The game got playable

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from "react";
+import { calculateWinner } from "../helpers";
+import Board from "./Board";
+
+const styles = {
+  width: "200px",
+  margin: "200px auto",
+};
 
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  const clickHandler = (i) => {
+    const boardCopy = [...board];
+    if (winner || boardCopy[i]) return;
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = () => {};
+
+  const renderMoves = () => {
     return (
-        <div>
-            Game
-        </div>
+      <button
+        onClick={() => {
+          setBoard(Array(9).fill(null));
+        }}
+      >
+        Restart Game
+      </button>
     );
+  };
+  return (
+    <>
+      <Board squares={board} onClick={clickHandler} />
+      <div style={styles}>
+        <p>
+          {winner ? `winner: ${winner}` : `Next Player: ${xIsNext ? "X" : "O"}`}
+        </p>
+        {renderMoves()}
+      </div>
+    </>
+  );
 };
 
 export default Game;
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Square = () => {
+const style = {
+    background: "lightblue",
+    border: "2px solid darkblue",
+    fontSize: "30px",
+    fontWeight: "800",
+    cursor: "pointer",
+    outline: "none",
+}
+
+const Square = ({ value, onClick }) => {
     return (
-        <div>
-            Square
-        </div>
+        <button onClick={onClick} style={style}>
+            {value}
+        </button>
     );
 };
 
